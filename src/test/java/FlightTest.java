fd673532c45f9bb5f1b1f6ac3a9b6fe323d89cdd
m6ttw <matthewrhyswilliams@live.com>
2021-02-12 13:24:50 +0000

    all flight getters pass

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -5,14 +5,43 @@ import static org.junit.Assert.assertEquals;
 
 public class FlightTest {
     Flight flight;
+    Plane plane;
+    Pilot pilot;
 
     @Before
     public void before(){
-        flight = new Flight("FR756", "MAD", "BHX", "06:25", new Plane(PlaneType.BOEING737, PlaneType.BOEING737, PlaneType.BOEING737), new Pilot("Chris", Rank.CAPTAIN, "ABC12345"));
+        plane = new Plane(PlaneType.BOEING737, PlaneType.BOEING737, PlaneType.BOEING737);
+        pilot = new Pilot("Chris", Rank.CAPTAIN, "ABC12345");
+        flight = new Flight("FR756", "MAD", "BHX", "06:25", plane, pilot);
     }
 
     @Test
     public void hasFlightNumber(){
         assertEquals("FR756", flight.getFlightNumber());
+    }
+
+    @Test
+    public void hasDestination(){
+        assertEquals("MAD", flight.getDestination());
+    }
+
+    @Test
+    public void hasDepartureAirport(){
+        assertEquals("BHX", flight.getDepartureAirport());
+    }
+
+    @Test
+    public void hasDepartureTime(){
+        assertEquals("06:25", flight.getDepartureTime());
+    }
+
+    @Test
+    public void hasPlane(){
+        assertEquals(plane, flight.getPlane());
+    }
+
+    @Test
+    public void hasPilot(){
+        assertEquals(pilot, flight.getPilot());
     }
 }
